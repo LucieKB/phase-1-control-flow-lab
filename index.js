d20716d438673d2d86b1b8ababec1b9e02139bb7
Lucie Kayser-Bril <l.kayserbril@gmail.com>
2022-11-22 06:04:45 -0700

    commit tree 85fc5f15128fabc389c2379f9780988350e0e817

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength){
+  let greeting ;
+if (rideLength <= 400){
+  greeting = 'This one is on me!'
+  return greeting
+} else if (rideLength <= 2000){
+  greeting = 'That will be twenty bucks.'
+  return greeting
+} else if (rideLength <= 2500) {
+  greeting = 'I will gladly take your thirty bucks.'
+  return greeting
+} else if (rideLength > 2500){
+  greeting = 'No can do.'
+  return greeting
+}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let answer  ;
+city === "NYC"?(answer = "Ok, sounds good."):(answer = "No go.")
+return answer
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+ let answer;
+switch(tipAmount){
+  case 'generous':
+    answer = "Thank you so much."
+    return answer
+    break;
+  case 'not as generous' :
+      answer = "Thank you."
+      return answer
+      break;
+  default :
+    answer = "Bye."
+    return answer
+    break;
+}
 }
